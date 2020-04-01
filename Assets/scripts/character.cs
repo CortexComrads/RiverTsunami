4bdc5c2a787a3d8e4773c41f97aeef7e1949b9fb
ingalipt <nikitka960@gmail.com>
2020-04-01 19:39:25 +0300

    to fill the void

---
+++ Assets/scripts/character.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NVIDIA.Flex;
+
+public class character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(10, 0, 0));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
