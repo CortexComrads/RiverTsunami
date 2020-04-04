34c8b3eadd1236fbd9dc59a127edb44df13f03bc
ingalipt <nikitka960@gmail.com>
2020-04-04 04:46:18 +0300

    project update

--- Assets/scripts/character.cs
+++ Assets/scripts/character.cs
@@ -1,20 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using NVIDIA.Flex;
+
 
 public class character : MonoBehaviour
 {
+
+    
+    
+   
+
+    
+
+
+
+    [SerializeField]
+
+    public float forward_speed;
+    public float player_speed;
+    public DynamicJoystick dynamicJoystick;
+
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        this.GetComponent<Rigidbody>().AddForce(new Vector3(10, 0, 0));
-        
+        rb = this.GetComponent<Rigidbody>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void FixedUpdate()
+    {
+        
+        rb.velocity = new Vector3(forward_speed, rb.velocity.y, rb.velocity.z);//move forward
+
+        Vector3 direction = Vector3.right * dynamicJoystick.Vertical + Vector3.forward * dynamicJoystick.Horizontal;//read joystick
+        rb.AddForce(direction * player_speed * Time.fixedDeltaTime, ForceMode.VelocityChange);//right-left acceleration
+
+
+
     }
 }
--- Assets/scripts/game_controller.cs
+++ Assets/scripts/game_controller.cs
@@ -6,16 +6,19 @@ public class game_controller : MonoBehaviour
 {
     [SerializeField]
     public GameObject[] chunks;
-    
+    public GameObject object112;
+    public Material material;
+    Vector2 river_offset;
     // Start is called before the first frame update
     void Start()
     {
       
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //object112.GetComponentInChildren<Renderer>().sharedMaterial.DisableKeyword("_EMISSION");
     }
 }
