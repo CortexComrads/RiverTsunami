e74ee3ade73113e07e7e75ee5bb8affc128192be
ingalipt <nikitka960@gmail.com>
2020-04-04 04:52:44 +0300

    project update 2

---
+++ Assets/scripts/govno.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class govno : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public GameObject govneco;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = govneco.transform.position;  
+    }
+}
